640141891274bef1aae2257d609576cdecb594aa
U <Command2020@mail.ru>
2023-04-28 19:17:33 +0300

    Initial commit

--- lms_project/learning/urls.py
+++ lms_project/learning/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', index, name='index'),
     path('create/', create, name='create'),  # Параметр name позволяет принимать обратный поиск адресов.
     path('delete/<int:course_id>/', delete, name='delete'),
-    re_path(r'^detail/(?P<course_id>[1-9])/$', detail, name='detail'),
+    re_path(r'^detail/(?P<course_id>\d+)/$', detail, name='detail'),
     path('enroll/<int:course_id>/', enroll, name='enroll'),
 ]
 # Рабочий вариант! r'^detail/(?P<course_id>\d+)/$ - бесконечный хоть до 100.
--- lms_project/lms_project/settings.py
+++ lms_project/lms_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'lms_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,8 +115,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static/img',
+    BASE_DIR / 'static/styles',
+    BASE_DIR / 'static/scripts',
+]
 
 # Media files
 # MEDIA_ROOT — это параметр в Django, который указывает полный путь к файловой системе, где будут храниться загруженные файлы.
