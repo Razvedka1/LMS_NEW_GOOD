a8ee193c2a8f57d3d6e6e8d28447469a45279a1e
U <Command2020@mail.ru>
2023-05-12 22:20:57 +0300

    Initial commit

--- lms_project/learning/forms.py
+++ lms_project/learning/forms.py
@@ -5,7 +5,7 @@ from django import forms
 class CourseForm(forms.ModelForm):
     class Meta:
         model = Course
-        fields = ('title', 'description', 'start_date', 'duration', 'price', 'count_lessons')
+        fields = ('title', 'description', 'start_date', 'duration', 'price', 'count_lessons',)
 
 class ReviewForm(forms.ModelForm):
     class Meta:
